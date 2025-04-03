73647243330691103c8c3a7019fd666c656967d9
seamuskills <thedarkknite10@gmail.com>
2025-04-03 13:26:18 -0500

    debug template and size adjustments

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ except FileNotFoundError:
     print("Image file could not be found.")
     sys.exit()
 
+debug = "template" in sys.argv
+
 size = sys.argv[2][0] if len(sys.argv) >= 3 else 'M'
 print("target card size %s"%(size))
 outPath = sys.argv[3] if len(sys.argv) >= 4 else os.path.dirname(sys.argv[1])
@@ -22,6 +24,11 @@ else:
 print("original size %dx%d"%(image.size[0], image.size[1]))
 
 y = 0
+
+size = size.upper()
+if not size in ["S", "M", "L"]:
+    size = "M"
+    print("invalid size. defaulting to M")
 
 match (size):
     case 'S':
@@ -37,7 +44,12 @@ x = round((y/image.size[1]) * image.size[0])
 image = image.resize((x, y))
 print("new size %sx%s"%(image.size[0], image.size[1]))
 
-final = Image.new(image.mode, (image.size[0], image.size[1] * 2), (0, 0, 0, 0))
-final.paste(image, (0, 0))
+final = Image.new(image.mode, (image.size[0], image.size[1] * 3), (0, 0, 0, 0))
+if debug:
+    final = Image.open("cardTemplate.png")
+    final.paste(image, (476 - image.size[0]//2, 937 - image.size[1]))
+    print("using debug template.")
+else:
+    final.paste(image, (0, 0))
 
 final.save(os.path.join(outPath, "output.png"))
