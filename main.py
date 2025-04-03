df3c8994c492a0daf69bdef65256007f3b361d62
seamuskills <thedarkknite10@gmail.com>
2025-04-03 12:04:03 -0500

    version 1

---
+++ main.py
@@ -0,0 +1,42 @@
+from PIL import Image
+import os
+import sys
+
+if (len(sys.argv) <= 1):
+    print("arguments: <image path> [size (S/M/L)] [output directory]")
+
+try:
+    image = Image.open(sys.argv[1])
+except FileNotFoundError:
+    print("Image file could not be found.")
+    sys.exit()
+
+size = sys.argv[2][0] if len(sys.argv) >= 3 else 'M'
+print("target card size %s"%(size))
+outPath = sys.argv[3] if len(sys.argv) >= 4 else os.path.dirname(sys.argv[1])
+if (outPath == ''):
+    print("target directory %s"%(os.getcwd()))
+else:
+    print("target directory %s"%(outPath))
+
+print("original size %dx%d"%(image.size[0], image.size[1]))
+
+y = 0
+
+match (size):
+    case 'S':
+        y = 200
+    case 'M':
+        y = 250
+    case 'L':
+        y = 300
+
+#this decides with based on height while keeping the existing aspect ratio of the image.
+x = round((y/image.size[1]) * image.size[0])
+
+image = image.resize((x, y))
+print("new size %sx%s"%(image.size[0], image.size[1]))
+
+final = Image.new(image.mode, (image.size[0], image.size[1] * 2), (0, 0, 0, 0))
+final.paste(image, (0, 0))
+final.show()
